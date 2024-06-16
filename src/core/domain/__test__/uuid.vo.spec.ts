42dd14caa4950065f4e433fa251d0f8a6e0798ec
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-06-16 14:24:50 +0000

    test(vo uuid): implementando teste para validar o static create do uuid vo

--- src/core/domain/__test__/uuid.vo.spec.ts
+++ src/core/domain/__test__/uuid.vo.spec.ts
@@ -2,15 +2,17 @@ import { InvalidUuidError } from "../../errors/invalid-uuid-error"
 import { Uuid } from "../uuid.vo"
 
 describe('Uuid Unit Test', () => {
+  const spy = jest.spyOn(Uuid.prototype as any, 'validate')
 
   describe('Error cases', () => {
     it('Deve retornar um erro caso o id seja inválido', () => {
       expect(() => new Uuid('invalid_id')).toThrow(InvalidUuidError)
+      expect(spy).toHaveBeenCalled()
+
     })
   })
   describe('Success cases', () => {
     it('Deve criar um id válido caso nenhum id seja passado', () => {
-      const spy = jest.spyOn(Uuid.prototype as any, 'validate')
       const uuid = new Uuid()
       expect(uuid.id).toBeTruthy()
       expect(spy).toHaveBeenCalled()
@@ -19,6 +21,12 @@ describe('Uuid Unit Test', () => {
     it('Deve criar um id válido caso um id seja passado', () => {
       const uuid = new Uuid('c3b4e5e9-9c7e-4c4c-8b1d-4f2b4b0d9d9f')
       expect(uuid.id).toBe('c3b4e5e9-9c7e-4c4c-8b1d-4f2b4b0d9d9f')
+    })
+
+    it('Deve criar um id pelo método estático create', () => {
+      const uuid = Uuid.create()
+      expect(uuid.id).toBeTruthy()
+      expect(spy).toHaveBeenCalled()
     })
   })
 })
